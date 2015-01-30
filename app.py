8eb85e79473630a2067eb5c7a13b7f980dd1ed2f
Collin Johnston <collin389@gmail.com>
2015-01-29 16:05:31 -0800

    Test

--- app.py
+++ app.py
@@ -12,5 +12,10 @@ def hello_name(name):
     """ Takes a name and greets them """
     return "Hello %s!" % name
 
+@app.route('/test/<name>')
+def hello_test(name):
+    """ Takes a name and greets them """
+    return "Hello %s!" % name
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5050)
