f91e4405c9a1f093c5e9f22b7d2d19a29e629541
Collin Johnston <collin389@gmail.com>
2015-01-31 00:17:20 -0800

    Added a nbunch of stuff:

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from flask import Flask
+from flask import send_from_directory
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
     """ Another Comment """
-    return "Hello World!"
+    return send_from_directory('', 'index.html')
 
-@app.route('/<name>')
-def hello_name(name):
-    """ Takes a name and greets them """
-    return "Hello %s!" % name
+@app.route('/static/<path:filename>')
+def send_static(filename):
+    return send_from_directory('static/', filename)
 
-@app.route('/test/<name>')
-def hello_test(name):
-    """ Takes a name and greets them """
-    return "Hello %s!" % name
+@app.route('/views/<path:filename>')
+def send_views(filename):
+    return send_from_directory('views/', filename)
+
+@app.route('/js/<path:filename>')
+def send_js(filename):
+    return send_from_directory('js/', filename)
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5050)
